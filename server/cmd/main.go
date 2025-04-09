4dff769d635c4aacc2335b899af7d60908406ee4
Jesstin Swadley <contact@jesstin.com>
2025-04-08 22:32:50 -0700

    create POST route for products

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /products", handleProducts)
+
+	fmt.Println("Server listening to Port 8080")
+	http.ListenAndServe(":8080", mux)
+}
+
+func handleProducts(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from products")
 }
